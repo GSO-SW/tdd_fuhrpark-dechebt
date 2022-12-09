42943970b1047ca856e24ed6bad9d4f9db187896
Salih Azad Salih Salih <salihazad.s@SCHULE.KOELN>
2022-12-09 12:36:13 +0100

    Program fertig.

---
+++ Fuhrparkverwaltung/Auto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuhrparkverwaltung
+{
+    public class Auto
+    {
+
+        private int kilometerstand;
+
+        public Auto(int kilometerstand)
+        {
+            this.kilometerstand = kilometerstand;
+        }
+        public int Kilometerstand
+        {
+            get
+            {
+                return this.kilometerstand;
+            }
+        }
+        public void Fahren(int streckeinkiklometer)
+        {
+            this.kilometerstand += streckeinkiklometer;
+        }
+    }
+}
